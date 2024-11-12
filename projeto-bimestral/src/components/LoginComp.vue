6edd4f22d88979c7faf59d75e57b06f8e0a7a0e3
Luiz Felipe de Queiroz <143653175+LuizFelipedeQueiroz@users.noreply.github.com>
2024-11-12 10:56:27 -0300

    Tela de login com HTML e CSS
    Pode customizar o HTML e o CSS no comentário

--- projeto-bimestral/src/components/LoginComp.vue
+++ projeto-bimestral/src/components/LoginComp.vue
@@ -1,3 +1,146 @@
+<!-- Use esse HTML e CSS aqui  -->
+
+<!-- CSS 
+<style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 95vh;
+            background-color: rgb(255, 255, 255);
+            background-size: cover;
+            background-position: center;
+        }
+
+        .wrapper{
+            background: transparent;
+            width: 620px;
+            color: rgb(0, 0, 0);
+            border: 2px solid white;
+            box-shadow: 0 0 10px rgb(39, 39, 39);
+            border-radius: 10px;
+            padding: 30px 40px;
+            backdrop-filter: blur(20px);
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .wrapper h1 {
+            font-size: 36px;
+            text-align: center;
+        }
+
+        .wrapper .input-box{
+            position: relative;
+            width: 100%;
+            height: 50px;
+            margin: 30px 0;
+        }
+
+        .input-box input{
+            width: 100%;
+            height: 100%;
+            background: transparent;
+            border: none;
+            outline: none;
+            border: 2px solid rgb(0, 0, 0);
+            border-radius: 40px;
+            font-size: 16px;
+            color: rgb(0, 0, 0);
+            padding: 20px 45px 20px 20px;
+        }
+
+        .input-box input::placeholder{
+            color: rgb(0, 0, 0);
+        }
+
+        .input-box i {
+            position: absolute;
+            right: 20px;
+            top: 50%;
+            transform: translateY(-50%);
+            font-size: 20px;
+        }
+
+        .wrapper .forget{
+            display: flex;
+            justify-content: space-between;
+            font-size: 15px;
+            margin: -15px 0 15px;
+        }
+
+        .forget label input {
+            accent-color: rgb(0, 0, 0);
+            margin-right: 3px;
+        }
+
+        .forget a{
+            color: rgb(0, 0, 0);
+            text-decoration: none;
+        }
+        
+        .forget a:hover{
+            text-decoration: underline;
+        }
+
+        .wrapper .btn{
+            width: 100%;
+            height: 45px;
+            background: white;
+            border: none;
+            outline: none;
+            border-radius: 40px;
+            box-shadow: 0 0 10px rgb(0, 0, 0);
+            cursor: pointer;
+            font-size: 16px;
+            color: black;
+            font-weight: 600px;
+        }
+
+        .wrapper .sing-up{
+            font-size: 15px;
+            text-align: center;
+            margin: 20px 0 15px;
+        }
+
+        .sing-up a{
+            color: rgb(0, 0, 0);
+            text-decoration: none;
+            font-weight: 600px;
+        }
+
+        .sing-up a:hover{
+            text-decoration: underline;
+        } -->
+
+       <!--  HTML
+    <div class="wrapper">
+        <form action="">
+            <h1>Logar</h1>
+            <div class="input-box">
+                <input type="text" placeholder="E-mail" required>
+                <i class='bx bxs-user-circle'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+                <i class='bx bxs-lock'></i>
+            </div>
+            <div class="forget">
+                <label><input type="checkbox">Esqueceu a senha?</label><a href="">clique aqui</a>
+            </div>
+            <button type="submit" class="btn">Entrar</button>
+            <div class="sing-up">
+                Não tem uma senha? <a href="">clique aqui</a>
+            </div>
+        </form>
+    </div>
+-->
+
 <template>
     <h2>LOGIN</h2>
     <div class="caixinhas">
