1a3916308df27a651dda42e0d0d0e4b81a4f3d64
khaleusanches <khaleusanches@gmail.com>
2024-11-23 01:58:16 -0300

    Instalado os modulos da api

---
+++ API-ProjetoBimestral/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using API_ProjetoBimestral.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ProjetoBimestral.Controllers
+{
+    [ApiController]
+    [Route("api/clientes")]
+    public class ClienteController : Controller
+    {
+        ConnectionContextDB context = new ConnectionContextDB();
+        [HttpGet]
+        public async Task<IActionResult> getUsers()
+        {
+            var clientes = await context.Cliente.ToListAsync();
+            return Ok(clientes);
+        }
+    }
+}
---
+++ API-ProjetoBimestral/Data/ConnectionContextDB.cs
@@ -0,0 +1,18 @@
+﻿using API_ProjetoBimestral.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_ProjetoBimestral.Services
+{
+    public class ConnectionContextDB : DbContext
+    {
+        public DbSet<ClienteModel> Cliente { get; set; }
+        public DbSet<PedidoModel> Pedido { get; set; }
+        public DbSet<VendedorModel> Vendedor { get; set; }
+        public DbSet<LivroModel> Livro { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=Bibliotecass;Uid=root;Pwd=123;", new MySqlServerVersion(new Version(8, 0, 21)));
+        }
+    }
+}
