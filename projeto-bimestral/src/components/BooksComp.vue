c77ac455f783e8c5cd0f1ff2b748c85a35be62cb
Luiz Felipe de Queiroz <143653175+LuizFelipedeQueiroz@users.noreply.github.com>
2024-11-29 17:10:27 -0300

    Componente dos livros melhorado
    Melhoria nos componentes.

--- projeto-bimestral/src/components/BooksComp.vue
+++ projeto-bimestral/src/components/BooksComp.vue
@@ -1,45 +1,52 @@
 <template>
 <div class="grade">
 <div>
-    <!-- Todos os preços não estão condizentes com os preços de mercados -->
 <img src="https://m.media-amazon.com/images/I/6132ndvQdiL._SY466_.jpg" alt="">
 <p>O Extraordinário</p>
-<p>R$35,80</p>  <!-- Preço corrigido: R$40,00 -->
+<p class="preço">R$40,00</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/91r5G8RxqfL._SY466_.jpg" alt="">
 <p>É assim que acaba</p>
-<p>R$26,10</p> <!-- Preço corrigido: R$44,95 -->
+<p class="preço">R$44,95</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/71fWaI5myqL._SY466_.jpg" alt="">
 <p>Diário de um Banana</p>
-<p>R$42,50</p> <!-- Preço corrigido: R$45,50 -->
+<p class="preço">R$45,50</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/51sqZ+7o-VL._SY445_SX342_.jpg" alt="">
 <p>O poder da Ação</p>
-<p>R$32,90</p> <!-- Preço corrigido: R$36,90 -->
+<p class="preço">R$36,90</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/81FEytag46L._SY466_.jpg" alt="">
 <p>O lado feio do amor</p>
-<p>R$86,70</p> <!-- Preço corrigido: R$55,41 (preço inflado demais)-->
+<p class="preço">R$55,41</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/715h8DWokhL._SY466_.jpg" alt="">
 <p>Ainda estou aqui</p>
-<p>R$120,35</p>  <!-- Preço corrigido: R$71,91 (preço inflado demais)-->
+<p class="preço">R$71,91</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/4128BOZ-OhL._SY445_SX342_.jpg" alt="">
 <p>Pessoas normais</p>
-<p>R$77,40</p>  <!-- Preço corrigido: R$119,99 (livro internacional tem que levar em consideração dólar e euro)-->
+<p class="preço">R$119,99</p>
+<button>Compre</button>
 </div>
 <div>
 <img src="https://m.media-amazon.com/images/I/81Vhnel+xxL._SY466_.jpg" alt="">
-<p>Todas as suas IMperfeições</p> 
-<p>R$48,90</p> <!-- Preço corrigido: R$70,40 (livro internacional tem que levar em consideração dólar e euro)-->
+<p class="nome">Todas as suas Imperfeições</p>
+<p class="preço">R$70,40</p>
+<button>Compre</button>
 </div>
 </div>
 </template>
@@ -51,6 +58,30 @@ export default {
 </script>
 
 <style>
+.grade > div{
+  padding: 1em;
+  border-radius: 10px;
+  box-shadow: 2px 2px 25px;
+  max-width: 18em;
+  margin-bottom: 2em;
+}
+
+.grade > div > .preço{
+  font-size: 30px;
+}
+
+.grade > div > .nome{
+  font-size: 16px;
+}
+
+.grade > div > button{
+  padding: 10px;
+  width: 250px;
+  background-color: rgb(69, 176, 142);
+  color: white;
+  border-radius: 5px;
+}
+
 .grade{
   margin-left: 50px;
   display: grid;
